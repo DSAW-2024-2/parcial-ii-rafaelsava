cfcd81a2ae6520f4fb09ed8ce3d90f580b0a4e16
Rafael Salcedo <rafaelsava@unisabana.edu.co>
2024-10-04 08:00:42 -0500

    preview final

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const app = express();
 app.use(express.json());
 
 const {router:loginRouter} = require('./routes/login');
+const {router:weatherRouter} = require('./routes/weather');
 
 app.get('/', (req, res) => {
     res.json({ message: 'Endpoint Funcionando' });
 });
 
 app.use('/login', loginRouter);
+app.use('/weather', weatherRouter);
 app.use((req,res)=>{
     res.status(404).json({error: 'Not Found'});
 });
